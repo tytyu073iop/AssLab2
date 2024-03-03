ba3420b6c2348af53daafc2297d7021edbb8d7d8
ILYA <ilya.biryuk2005@icloud.com>
2024-03-03 13:39:21 +0300

    final

--- AssLab2/Biryuk_2.cpp
+++ AssLab2/Biryuk_2.cpp
@@ -3,9 +3,9 @@
 int main() {
 	const int N = 20;
 	int ma;
-	int a[N]{8, 4, 6, 7, 5, 1, 9, 0, 5, 6, 3, 7, 9, 5, 0, 4, 0, 1 ,2, 3};
+	int a[N]{8, 4, 6, 7, 5, 1, 9, 0, 5, 6, 3, 0, 9, 5, 0, 4, 0, 1 ,2, 3};
 	// count yourself
-	// 0 - 3, 1 - 2, 2 - 1, 3 - 2, 4 - 2, 5 - 3, 6 - 2, 7 - 2, 8 - 1, 9 - 2
+	// 0 - 4, 1 - 2, 2 - 1, 3 - 2, 4 - 2, 5 - 3, 6 - 2, 7 - 1, 8 - 1, 9 - 2
 	// max - 0, 5
 	_asm {
 		mov ecx, N
@@ -25,9 +25,9 @@ int main() {
 		dec ecx
 			cycle1:
 		mov eax, a[ecx*4-4]
-		mov ebx, 1
+		mov ebx, 0 // 1
 		push ecx
-		dec ecx
+		//dec ecx
 			cycle2:
 		cmp a[ecx * 4 - 4], eax
 		jne ne2
@@ -43,6 +43,9 @@ int main() {
 			loop cycle1
 		mov eax, [esp+4]
 		mov ma, eax
+		// empty stack
+		pop ebx
+		pop eax
 	}
 	std::cout << "Max meet is: " << ma << '\n';
 }
